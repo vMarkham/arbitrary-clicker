b25fca4cd61c01aeb484664b1aac91745ce98ac7
Vincent Markham <vincent.markham@gmail.com>
2016-03-07 19:50:35 -0800

    Refactor to include main game loop, add passive APV gain

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 var $apv = $('#apv-count');
-var clickMain = {
-  active: true,
-  clickValue: 1,
-  onClick: function() {
-    var int = parseInt($apv.text());
-    $apv.text(int + this.clickValue);
+var $fishermenCost = $('#fishermen-cost');
+var apvCount = 0;
+var clickPower = 1;
+
+var arbitryClick = function(number) {
+  apvCount = apvCount + number;
+  $apv.text(apvCount);
+}
+
+var fishermen = {
+  active: false,
+  cost: 15,
+  quantity: 0,
+  value: 1,
+  buyThis: function() {
+      if (apvCount >= this.cost) {
+        this.quantity += 1;
+        apvCount -= this.cost;
+        this.cost = Math.floor(15 * Math.pow(1.2, this.quantity));
+        $apv.text(apvCount);
+        $fishermenCost.text(this.cost);
+      }
   }
 }
 
-
 $("#click-main").on('click', function(event) {
-    clickMain.onClick();
+  arbitryClick(clickPower);
 })
+
+$('#fishermen-btn').on('click', function(event) {
+  fishermen.buyThis();
+})
+
+window.setInterval(function() {
+arbitryClick(fishermen.quantity*fishermen.value);
+}, 1000);
